b4e51f50fe4f19ee7d3aec677133eaaffad5c0a7
Isuru Ranasinghe <isuruziglesiaz@gmail.com>
2024-01-07 19:19:10 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// init Isotope
+var $grid = $('.collection-list').isotope({
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on('click', 'button', function () {
+  var filterValue = $(this).attr('data-filter');
+  resetFilterBtns();
+  $(this).addClass('active-filter-btn');
+  $grid.isotope({ filter: filterValue });
+});
+
+var filterBtns = $('.filter-button-group').find('button');
+function resetFilterBtns() {
+  filterBtns.each(function () {
+    $(this).removeClass('active-filter-btn');
+  });
+}
+
+
+const formOpenBtn = document.querySelector("#form-open"),
+  home = document.querySelector(".home"),
+  formContainer = document.querySelector(".form_container"),
+  formCloseBtn = document.querySelector(".form_close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login"),
+  pwShowHide = document.querySelectorAll(".pw_hide");
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+  });
+});
+
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
